5377565543f6b726653966d428a1e0bb488d7eb9
aqilizaaz <aqil.izaaz.21@student.se.ittelkom-sby.ac.id>
2022-12-14 15:26:13 +0700

    aqil

---
+++ proses_feedback.php
@@ -0,0 +1,9 @@
+<?php 
+    $kritik = $_POST['kritik'];
+    $saran = $_POST['saran'];
+    $rating = $_POST['rating'];
+
+    echo $kritik."</br>";
+    echo $saran."</br>";
+    echo $rating."</br>";
+?>
