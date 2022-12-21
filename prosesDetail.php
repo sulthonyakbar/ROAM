248e2f4a30c1b4c28cde9103fc053ebde2376f52
kresna59 <103045842+kresna59@users.noreply.github.com>
2022-12-22 00:46:00 +0700

    Update prosesDetail.php

--- prosesDetail.php
+++ prosesDetail.php
@@ -5,6 +5,7 @@ $alamat = $_POST['alamat'];
 $koordinat1 = $_POST['koordinat1'];
 $koordinat2 = $_POST['koordinat2'];
 
+
 $detail = new Detail($alamat, $koordinat1, $koordinat2);
 $detail->addDetail();
 ?>
