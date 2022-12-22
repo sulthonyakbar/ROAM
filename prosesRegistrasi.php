d2a16dec0bba8067d0d83c6ee307281a5ef04ea4
akbarrizkip <107041431+AkbarRizkiP@users.noreply.github.com>
2022-12-22 07:06:22 +0700

    Update prosesRegistrasi.php

--- prosesRegistrasi.php
+++ prosesRegistrasi.php
@@ -4,7 +4,6 @@ require("PHP/User.php");
 $nama = $_POST['nama'];
 $username = $_POST['username'];
 $password = $_POST['password'];
-
 $registrasi = new User($nama, $username, $password);
 $registrasi->Registrasi();
 ?>
